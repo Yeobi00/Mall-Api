f712b2231f5f5e7edf070e821d3ff839e37d7c11
yeobi <kimhyjjang@gmail.com>
2024-07-10 14:17:42 +0900

    fix config/settings.py CORS_ALLOWED_ORIGINS

--- config/settings.py
+++ config/settings.py
@@ -134,6 +134,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_ALLOW_ALL = False
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:3000',
+    'http://localhost:3000',
     'http://13.237.11.57'
 ]
